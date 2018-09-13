d155723d368eb50ba6133472c1d2fe6af2300bd3
Michael Sime <ms255@st-andrews.ac.uk>
2018-09-13 11:51:09 +0100

    Completed up to Services, HTTP and Observables

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [
-    AppComponent
-  ],
   declarations: [
     AppComponent
-  ]
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    PassengerDashboardModule
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Containers
+import { PassengerDashboardComponent } from './containers/passenger-dashboard/passenger-dashboard.component';
+
+// Components
+import { PassengerCountComponent } from './components/passenger-count/passenger-count.component';
+import { PassengerDetailComponent } from './components/passenger-detail/passenger-detail.component';
+
+@NgModule({
+  declarations: [
+    PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PassengerDashboardComponent // only export root component
+  ]
+})
+export class PassengerDashboardModule {}
